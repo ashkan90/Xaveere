623bac521c6f62438d06a624d3df029ad523b946
Emirhan <emirhan.1ataman@gmail.com>
2019-02-25 01:08:54 +0300

    Update profile.php

--- application/controllers/profile.php
+++ application/controllers/profile.php
@@ -1,35 +1,62 @@
 <?php 
 
-
+include "../system/libraries/request.php"; 
+// will be used 'use' keyword as soon as possible in future.
 /**
  * 
  */
 class Profile extends Xaveere
 {
-	
+	private $store_model;
 	function __construct()
 	{
-		//echo "Profile Controller";
+		parent::__construct();
+		$this->store_model = $this->model("users");
 	}
 
 	public function index()
 	{
-		echo "index";
+		$data = $this->store_model->fetch_records();
+		$this->view("index", $data);
+
 	}
 
 	public function method()
 	{
-		$title = "Xaveere Title";
-		$model = $this->model("users");
-		$this->helpers(["url", "form"]);
-		
-		//$model->fetch_records();
 
 		$this->view("user");
 	}
 
-	public function testMethod()
+	public function form_submit()
 	{
-		return print_r("asşldkasşd");
+
+		/*$name = $_POST['name'];
+		$address = $_POST['surname'];
+
+		$user = [
+			'name' => $name,
+			'address' => $address
+		];
+		
+		Old style
+
+		*/
+
+		
+		//$request = Request::all();
+		//New style.
+		
+
+		$request = new Request;
+		$request->validate([
+			'password' => 'required',
+			'password_confirm' => 'confirm|password|required'
+		]);
+
+		die();
+		if($this->store_model->create($request))
+			redirect("profile/method");
+		else
+			echo "fail";
 	}
 }
