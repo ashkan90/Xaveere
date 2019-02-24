ad3451ada8a0d6fc3566b342427e29b035474ba6
Emirhan <emirhan.1ataman@gmail.com>
2019-02-25 01:07:15 +0300

    Create request.php
    Request::all() // will be taken what data sent.

---
+++ system/libraries/request.php
@@ -0,0 +1,36 @@
+<?php 
+
+/**
+ * 
+ */
+class Request
+{
+	use Validation;
+	function __construct()
+	{
+		foreach($_POST ?? $_GET as $key => $value):
+		  $this->{$key} = $value;
+		endforeach;
+	}
+
+	public function all() : array
+	{
+		return ($_POST) ?? ($_GET);
+	}
+
+	public function uri($segment = 2) : string
+	{
+		if (isset($_GET['url'])) {
+			// Get Requested URL.
+			$url = $_GET['url'];
+
+			// Remove extra spaces from right side of link.
+			$url = rtrim($url);
+
+			// Remove all illegal characters from URL.d
+			$url = filter_var($url, FILTER_SANITIZE_URL);
+			$url = explode("/", $url);
+			return $url[$segment];
+		}
+	}
+}
